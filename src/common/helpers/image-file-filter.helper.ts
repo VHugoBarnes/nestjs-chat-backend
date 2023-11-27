4de4927a2c835334ab21893db3306c0e6c7e70b0
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-11-27 17:20:13 -0600

    update the name of file filter file

--- src/common/helpers/image-file-filter.helper.ts
+++ src/common/helpers/image-file-filter.helper.ts
@@ -1,6 +1,6 @@
 type cbType = (error: Error, acceptFile: boolean) => void;
 
-export const fileFilter = (req: Express.Request, file: Express.Multer.File, cb: cbType) => {
+export const imageFileFilter = (req: Express.Request, file: Express.Multer.File, cb: cbType) => {
   if (!file) return cb(new Error("File is empty"), false);
 
   const fileExtension = file.mimetype.split("/")[1];
