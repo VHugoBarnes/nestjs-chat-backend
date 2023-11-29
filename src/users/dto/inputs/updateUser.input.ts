66b3d82b5ebc564ab90f73a2b9f500c9c6b71c98
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-11-29 17:16:29 -0600

    remove swagger doc in non-rest modules

--- src/users/dto/inputs/updateUser.input.ts
+++ src/users/dto/inputs/updateUser.input.ts
@@ -1,5 +1,4 @@
 import { Field, InputType } from "@nestjs/graphql";
-import { ApiProperty } from "@nestjs/swagger";
 
 import { IsString, MinLength } from "class-validator";
 
@@ -8,12 +7,10 @@ export class UpdateUserInput {
   @IsString()
   @MinLength(1)
   @Field(() => String)
-  @ApiProperty()
   name: string;
 
   @IsString()
   @MinLength(1)
   @Field(() => String)
-  @ApiProperty()
   username: string;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,38 +1,31 @@
 import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
 import { Field, ID, ObjectType } from "@nestjs/graphql";
 import { Schema as MSchema } from "mongoose";
-import { ApiProperty } from "@nestjs/swagger";
 
 @Schema()
 @ObjectType()
 export class User {
   @Prop({ type: MSchema.Types.ObjectId })
   @Field(() => ID)
-  @ApiProperty()
   _id: string;
 
   @Prop({ required: true })
   @Field(() => String)
-  @ApiProperty()
   name: string;
 
   @Prop({ unique: true })
   @Field(() => String)
-  @ApiProperty()
   email: string;
 
   @Prop()
-  @ApiProperty()
   password: string;
 
   @Prop()
   @Field(() => String, { nullable: true })
-  @ApiProperty()
   profilePhoto: string;
 
   @Prop({ unique: true })
   @Field(() => String)
-  @ApiProperty()
   username: string;
 }
 
