5219a1bc6f546672e457f5f44fb309281b2b23ed
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-11-29 17:30:45 -0600

    imports in chat-messages

--- src/chat-messages/chat-messages.module.ts
+++ src/chat-messages/chat-messages.module.ts
@@ -1,8 +1,23 @@
 import { Module } from "@nestjs/common";
 import { ChatMessagesService } from "./chat-messages.service";
 import { ChatMessagesResolver } from "./chat-messages.resolver";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ChatMessage, ChatMessageSchema } from "./entities/chat-message.entity";
+import { ChatModule } from "src/chat/chat.module";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
-  providers: [ChatMessagesResolver, ChatMessagesService],
+  providers: [
+    ChatMessagesResolver,
+    ChatMessagesService
+  ],
+  imports: [
+    MongooseModule.forFeature([{ name: ChatMessage.name, schema: ChatMessageSchema }]),
+    ChatModule,
+    UsersModule,
+  ],
+  exports: [
+    ChatMessagesService,
+  ]
 })
 export class ChatMessagesModule { }
